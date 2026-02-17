d836bd489fd06ceb18495a9d92f893ae0e00971b
Leandro Gonzalez <leanlag@gmail.com>
2026-02-17 19:20:09 -0300

    refactor(AdminDelete.jsx, AdminNav.jsx, AdminUpdate.jsx): mejorar la legibilidad del código y corregir rutas de importación

--- client/src/components/admincomponents/AdminNav.jsx
+++ client/src/components/admincomponents/AdminNav.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import '../../pages/AdminProfile.css';
+import { NavLink } from "react-router-dom";
+import "../../pages/admin/AdminProfile.css";
 
 export const AdminNav = () => {
   return (
-    <nav className="profile-nav-admin" style={{ display: 'flex', gap: 8, marginBottom: 12 }}>
-      <NavLink to="/admin/insert" className={({isActive})=> isActive ? 'btn-primary' : 'btn-secondary'}>Agregar producto</NavLink>
-      <NavLink to="/admin/update" className={({isActive})=> isActive ? 'btn-primary' : 'btn-secondary'}>Actualizar producto</NavLink>
-      <NavLink to="/admin/delete" className={({isActive})=> isActive ? 'btn-primary' : 'btn-secondary'}>Eliminar producto</NavLink>
-      <NavLink to="/admin/orders" className={({isActive})=> isActive ? 'btn-primary' : 'btn-secondary'}>
-              Pedidos
-            </NavLink>
+    <nav
+      className="profile-nav-admin"
+      style={{ display: "flex", gap: 8, marginBottom: 12 }}
+    >
+      <NavLink
+        to="/admin/insert"
+        className={({ isActive }) =>
+          isActive ? "btn-primary" : "btn-secondary"
+        }
+      >
+        Agregar producto
+      </NavLink>
+      <NavLink
+        to="/admin/update"
+        className={({ isActive }) =>
+          isActive ? "btn-primary" : "btn-secondary"
+        }
+      >
+        Actualizar producto
+      </NavLink>
+      <NavLink
+        to="/admin/delete"
+        className={({ isActive }) =>
+          isActive ? "btn-primary" : "btn-secondary"
+        }
+      >
+        Eliminar producto
+      </NavLink>
+      <NavLink
+        to="/admin/orders"
+        className={({ isActive }) =>
+          isActive ? "btn-primary" : "btn-secondary"
+        }
+      >
+        Pedidos
+      </NavLink>
     </nav>
   );
 };
