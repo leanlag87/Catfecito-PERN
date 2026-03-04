1048c31a17918c13d9619ec372c656fa9f22d0ba
Leandro Gonzalez <leanlag@gmail.com>
2026-03-03 22:35:48 -0300

    feat(auth.hooks): agregar archivo de exportación para hooks de autenticación

---
+++ client/src/features/auth/hooks/index.js
@@ -0,0 +1,5 @@
+// Archivo de exportación de hooks relacionados con la autenticación
+export { useAuth } from "./useAuth";
+export { useRequireAuth } from "./useRequireAuth";
+export { useRequireRole } from "./useRequireRole";
+export { useAuthForm } from "./useAuthForm";
