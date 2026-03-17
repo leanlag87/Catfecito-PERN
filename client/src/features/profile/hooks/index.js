e514c804e26a9762d1ade24ec221143b6b0c1df6
Leandro Gonzalez <leanlag@gmail.com>
2026-03-17 15:42:24 -0300

    feat(profile): agregar archivo de índice para exportar hooks de perfil

---
+++ client/src/features/profile/hooks/index.js
@@ -0,0 +1,7 @@
+export { useProfile } from "./useProfile";
+export { useProfileAddress } from "./useProfileAddress";
+export { useProfileSecurity } from "./useProfileSecurity";
+export { useProfileOrders } from "./useProfileOrders";
+export { useProfileForm } from "./useProfileForm";
+export { useProfileNavigation } from "./useProfileNavigation";
+export { useProfileBootstrap } from "./useProfileBootstrap";
