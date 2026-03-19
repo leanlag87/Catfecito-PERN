906a6b1787081630ae1fa94dc4af3739fd71817b
Leandro Gonzalez <leanlag@gmail.com>
2026-03-19 16:48:10 -0300

    feat(utils): agregar archivo de utilidades para formateo, manejo de URLs y almacenamiento

---
+++ client/src/shared/utils/index.js
@@ -0,0 +1,3 @@
+export * from "./format.utils"; // Formatea valores (moneda, fechas, texto, números)
+export * from "./url.utils"; // Construye y resuelve URLs absolutas/relativas e imágenes
+export * from "./storage.utils"; // Maneja localStorage/sessionStorage de forma segura
